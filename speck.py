51d7d7e75bf2ffa757b58bbdd35cb2d2b79ec584
River Gillis <jrivergillis@gmail.com>
2016-09-26 22:52:45 -0500

    Refactor into two large functions, update variable and function names to PEP8 standards

--- speck.py
+++ speck.py
@@ -1,34 +1,38 @@
 WORDLIST_FILENAME = "words.txt"
-with(open(WORDLIST_FILENAME, 'r')) as f:
-    content = [x.strip('\n') for x in f.readlines()]
-# content is now a list of every word in words.txt
-print("Loaded " + str(len(content)) + " words")
 
-inputString = input("Please 'swype' something: ")
-possible = []
-for word in content:
-    if len(word) >= 5:
-        prevLetter = ""
-        tempString = inputString
-        possibleFlag = True
-        for letter in word:
-            if letter != prevLetter: # get double letters for free
-                if letter not in tempString:
-                    possibleFlag = False
-                    #if (word == 'apple'):
-                    #    print(letter + " is not in " + tempString)
-                else:
-                    #if (word == 'apple'):
-                    #    print(str(len(tempString)) + " " + tempString + " " + str(inputString.index(letter)))
-                    if (len(tempString) - 1) != tempString.index(letter):
-                        tempString = tempString[tempString.index(letter)+1:]
+def load_wordlist(filename):
+    with(open(filename, 'r')) as f:
+        content = [x.strip('\n') for x in f.readlines()]
+    # content is now a list of every word in words.txt
+    print("Loaded " + str(len(content)) + " words")
+    return content
+
+def speck(input_string, content):
+    possible = []
+    for word in content:
+        if len(word) >= 5:
+            prev_letter = ""
+            temp_string = input_string
+            possible_flag = True
+            for letter in word:
+                if letter != prev_letter: # get double letters for free
+                    if letter not in temp_string:
+                        possible_flag = False
+                        #if (word == 'apple'):
+                        #    print(letter + " is not in " + tempString)
                     else:
-                        tempString = ''
-                    prevLetter = letter
-                
-            #tempString = tempString.replace(letter, "")
-        if possibleFlag:
-            possible.append(word)
-            print("**************FOUND " + word)
-betterPicks = [x for x in possible if x[0] == inputString[0] and x[-1] == inputString[-1]]
-print(betterPicks)
+                        #if (word == 'apple'):
+                        #    print(str(len(tempString)) + " " + tempString + " " + str(inputString.index(letter)))
+                        if (len(temp_string) - 1) != temp_string.index(letter):
+                            temp_string = temp_string[temp_string.index(letter)+1:]
+                        else:
+                            temp_string = ''
+                        prev_letter = letter
+                #tempString = tempString.replace(letter, "")
+            if possible_flag:
+                possible.append(word)
+                print("**************FOUND " + word)
+    better_picks = [x for x in possible if x[0] == input_string[0] and x[-1] == input_string[-1]]
+    return better_picks
+
+print(speck(input("Input: "), load_wordlist(WORDLIST_FILENAME)))
